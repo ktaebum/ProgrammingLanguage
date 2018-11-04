4e1f5068ee08a7f876a52411a4fa2cb07d7fe606
ktaebum <phya.ktaebum@gmail.com>
2018-11-05 01:05:46 +0900

    finished test3

--- hw5/SM5_skel/translate.ml
+++ hw5/SM5_skel/translate.ml
@@ -11,10 +11,36 @@ module Translator = struct
   let rec trans : K.program -> Sm5.command = function
     | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
+
+  (* Binding *)
     | K.LETV (x, e1, e2) ->
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
+    | K.LETF (f, x, e1, e2) ->
+      [Sm5.PUSH (Sm5.Fn (x, trans e1)); Sm5.BIND f] @
+      trans e2 @
+      [Sm5.UNBIND; Sm5.POP]
+
+  (* I/O *)
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
+    | K.WRITE e -> trans e @ [Sm5.PUT]
+
+  (* Variable Related *)
+    | K.VAR x -> [Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
+    | K.ASSIGN (x, e) -> 
+      trans e @
+      [Sm5.PUSH (Sm5.Id x); Sm5.STORE]
+
+  (* Execution *)
+    | K.SEQ (e1, e2) -> trans e1 @ trans e2
+
+  (* Call Function *)
+    | K.CALLV (f, arg) ->
+      [Sm5.PUSH (Sm5.Id f)] @
+      trans arg @
+      [Sm5.MALLOC; Sm5.CALL]
     | _ -> failwith "Unimplemented"
+      
+
 
 end
