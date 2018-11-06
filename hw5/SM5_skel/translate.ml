25ac157f8bef0540d34e7abdf8ad9a4c689ea410
ktaebum <phya.ktaebum@gmail.com>
2018-11-07 00:13:36 +0900

    change for return unit in for loop

--- hw5/SM5_skel/translate.ml
+++ hw5/SM5_skel/translate.ml
@@ -40,38 +40,18 @@ module Translator = struct
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
     | K.WRITE e -> 
       (* trans e should calculate integer *)
-      let anonymousFunc = "#f" in
       let anonymousArg = "#arg" in
-      let functionBody = 
-        trans e @
-        trans e
-      in
-      (* trans e @ [Sm5.PUT] *)
-      
-      (*
-      [Sm5.PUSH (Sm5.Fn (anonymousArg, [Sm5.BIND anonymousFunc] @
-                            functionBody @
-                            [Sm5.UNBIND; Sm5.POP])); Sm5.BIND anonymousFunc] @
-      *)
-      let rec afterPUT e = 
-        (match e with 
-         | K.WRITE e' -> afterPUT e'
-         | K.SEQ (e1, e2) ->
-           (match (e1, e2) with
-            | (K.WRITE e1', K.WRITE e2') -> K.SEQ (afterPUT e1', afterPUT e2')
-            | (_, K.WRITE e2') -> K.SEQ (e1, afterPUT e2')
-            | (K.WRITE e1', _) -> K.SEQ (afterPUT e1', e2)
-            | _ -> K.SEQ (e1, e2)
-           )
-         | _ -> e
-        )
-      in
       trans e @
-      (* trans (K.CALLV (anonymousFunc, e)) @ *)
+      [Sm5.MALLOC; Sm5.BIND anonymousArg; Sm5.PUSH (Sm5.Id anonymousArg); Sm5.STORE] @
+      [Sm5.PUSH (Sm5.Id anonymousArg); Sm5.LOAD] @
+      [Sm5.PUT] @
+      [Sm5.PUSH (Sm5.Id anonymousArg); Sm5.LOAD] @
+      [Sm5.UNBIND; Sm5.POP] 
+      (*
+      trans e @
       [Sm5.PUT]
-      (* @ trans (afterPUT e) *)
+         *)
 
-      (* [Sm5.UNBIND; Sm5.POP] *)
   (* Variable Related *)
     | K.VAR x -> [Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
     | K.ASSIGN (x, e) -> 
@@ -106,34 +86,50 @@ module Translator = struct
   (* Loop *)
     | K.FOR (i, e1, e2, e) ->
       let anonymousFunc = "#f" in
+      let anonymousBound = "#I" in
+      let anonymousIter = "#i" in
       let functionBody = 
-        (* increment i *)
+        (* update i and evaluate loop *)
+        [Sm5.PUSH (Sm5.Id anonymousIter); Sm5.LOAD] @
+        [Sm5.PUSH (Sm5.Id i); Sm5.STORE] @
         trans e @
-        [Sm5.PUSH (Sm5.Id i); Sm5.LOAD] @
+
+        (* check whether can go next step *)
+        [Sm5.PUSH (Sm5.Id anonymousIter); Sm5.LOAD] @
         [Sm5.PUSH (Sm5.Val (Sm5.Z 1))] @
         [Sm5.ADD] @
-
-        (* compare with e2 *)
-        trans (K.ADD (e2, K.NUM 1)) @
+        [Sm5.PUSH (Sm5.Id anonymousBound); Sm5.LOAD] @
+        [Sm5.PUSH (Sm5.Val (Sm5.Z 1))] @
+        [Sm5.ADD] @
         [Sm5.LESS] @
         [Sm5.JTR (
             (* assign and call function again *)
-            [Sm5.PUSH (Sm5.Id i); Sm5.LOAD] @
+            [Sm5.PUSH (Sm5.Id anonymousIter); Sm5.LOAD] @
             [Sm5.PUSH (Sm5.Val (Sm5.Z 1))] @
             [Sm5.ADD] @
-            [Sm5.PUSH (Sm5.Id i); Sm5.STORE] @
-            trans (K.CALLR (anonymousFunc, i)),
+            [Sm5.PUSH (Sm5.Id anonymousIter); Sm5.STORE] @
+            trans (K.CALLR (anonymousFunc, anonymousIter)),
+            (* Equal Case *)
             trans K.UNIT)]
       in
-      [Sm5.PUSH (Sm5.Fn (i, [Sm5.BIND anonymousFunc] @
+      trans e1 @
+      [Sm5.MALLOC; Sm5.BIND anonymousIter; Sm5.PUSH (Sm5.Id anonymousIter); Sm5.STORE] @
+      trans e2 @ 
+      [Sm5.MALLOC; Sm5.BIND anonymousBound; Sm5.PUSH (Sm5.Id anonymousBound); Sm5.STORE] @
+      [Sm5.PUSH (Sm5.Fn (anonymousIter, [Sm5.BIND anonymousFunc] @
                             functionBody @
                             [Sm5.UNBIND; Sm5.POP])); Sm5.BIND anonymousFunc] @
-      trans e1 @
-      trans (K.ADD (e2, K.NUM 1)) @
+      [Sm5.PUSH (Sm5.Id anonymousIter); Sm5.LOAD] @
+      [Sm5.PUSH (Sm5.Id anonymousBound); Sm5.LOAD] @
+      [Sm5.PUSH (Sm5.Val (Sm5.Z 1))] @
+      [Sm5.ADD] @
       [Sm5.LESS] @
-      [Sm5.JTR (trans e1 @
-                [Sm5.PUSH (Sm5.Id i); Sm5.STORE] @
-                trans (K.CALLR (anonymousFunc, i)), trans K.UNIT)] @ 
+      [Sm5.JTR ([Sm5.PUSH (Sm5.Id anonymousIter); Sm5.LOAD] @
+                [Sm5.PUSH (Sm5.Id anonymousIter); Sm5.STORE] @
+                trans (K.CALLR (anonymousFunc, anonymousIter))  
+                , trans K.UNIT)] @ 
+      [Sm5.UNBIND; Sm5.POP] @ 
+      [Sm5.UNBIND; Sm5.POP] @ 
       [Sm5.UNBIND; Sm5.POP]
     | K.WHILE (cond, e) ->
       let anonymousFunc = "#f" in
